e1de9c0e2ee3d21e230770d9f7dcf4f4a6062b0d
msmido9 <msmido9@gmail.com>
2021-07-03 13:19:34 +0200

    fix a bug

--- src/components/EndScreen.js
+++ src/components/EndScreen.js
@@ -3,15 +3,20 @@ import { useContext } from "react";
 import QuizContext from "../Helpers/Context";
 
 export default function EndScreen() {
-  const { score } = useContext(QuizContext);
+  const { score, setScore } = useContext(QuizContext);
   const { gameState, setGameState } = useContext(QuizContext);
+
+  const handleTryAgain = () => {
+    setScore(0);
+    setGameState("menu");
+  };
 
   return (
     <div>
       End Screen, your score is {score}
       <br />
       <button
-        onClick={() => setGameState("menu")}
+        onClick={handleTryAgain}
         className="bg-black px-2 py-1 text-white mt-2"
       >
         Try Again
