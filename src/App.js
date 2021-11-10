79f9157b59b8490baaec89ea25f73618ddbbb298
Mohamed Salah <msmido9@gmail.com>
2021-11-10 18:30:10 +0200

    add Procfile

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ import QuizContext from "./Helpers/Context";
 import Layout from "./components/Layout";
 
 function App() {
-  const [gameState, setGameState] = useState("menu");
+	const [gameState, setGameState] = useState("menu");
 
-  const [score, setScore] = useState(0);
+	const [score, setScore] = useState(0);
 
-  return (
-    <div className="App">
-      <h1 className="text-2xl py-8">Quiz App</h1>
+	return (
+		<div className="App">
+			<h1 className="text-2xl py-8">Quiz App</h1>
 
-      <QuizContext.Provider
-        value={{ gameState, setGameState, score, setScore }}
-      >
-        <Layout>
-          {gameState === "menu" && <MainMenu />}
-          {gameState === "quiz" && <Quiz />}
-          {gameState === "endScreen" && <EndScreen />}
-        </Layout>
-      </QuizContext.Provider>
-    </div>
-  );
+			<QuizContext.Provider
+				value={{ gameState, setGameState, score, setScore }}
+			>
+				<Layout>
+					{gameState === "menu" && <MainMenu />}
+					{gameState === "quiz" && <Quiz />}
+					{gameState === "endScreen" && <EndScreen />}
+				</Layout>
+			</QuizContext.Provider>
+		</div>
+	);
 }
 
 export default App;
