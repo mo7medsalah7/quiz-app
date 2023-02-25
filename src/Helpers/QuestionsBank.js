c75eccd82d28df909a5d4a8306463d004c5549c8
mo7medsalah7 <msmido9@gmail.com>
2023-02-25 04:15:45 +0200

    adding ignore files

---
+++ src/Helpers/QuestionsBank.js
@@ -0,0 +1,42 @@
+export const Questions = [
+  {
+    prompt: 'My Favorite School',
+    optionA: 'ITI',
+    optionB: 'Minia University',
+    optionC: 'Cairo University',
+    optionD: 'Edx',
+    answer: 'A',
+  },
+  {
+    prompt: 'What is Mo Salah fav social app',
+    optionA: 'Reddit',
+    optionB: 'Twitter',
+    optionC: 'Facebook',
+    optionD: 'tikTok',
+    answer: 'A',
+  },
+  {
+    prompt: 'My Name is Mohamed ....',
+    optionA: 'Sangha',
+    optionB: 'Sansa',
+    optionC: 'Salah',
+    optionD: 'Salem',
+    answer: 'C',
+  },
+  {
+    prompt: 'What is Docker',
+    optionA: 'Virtual Computer',
+    optionB: 'Container Engine',
+    optionC: 'Container Orchestrator',
+    optionD: 'Configuration Manager',
+    answer: 'B',
+  },
+  {
+    prompt: 'Mohamed Alaa Fav Cloud Provider',
+    optionA: 'Azure',
+    optionB: 'Linode',
+    optionC: 'AWS',
+    optionD: 'DigitalOcean',
+    answer: 'C',
+  },
+];
