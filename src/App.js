c30e58c2e484a68230283370bfadde339d975ab0
mo7medsalah7 <msmido9@gmail.com>
2023-02-25 04:58:30 +0200

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import { useState, useContext } from "react";
-import "./App.css";
-import MainMenu from "./components/MainMenu";
-import Quiz from "./components/Quiz";
-import EndScreen from "./components/EndScreen";
-import QuizContext from "./Helpers/Context";
-import Layout from "./components/Layout";
+import { useState, useContext } from 'react';
+import './App.css';
+import MainMenu from './components/MainMenu';
+import Quiz from './components/Quiz';
+import EndScreen from './components/EndScreen';
+import QuizContext from './Helpers/Context';
+import Layout from './components/Layout';
+import Navbar from './components/Navbar';
 
 function App() {
-	const [gameState, setGameState] = useState("menu");
+  const [gameState, setGameState] = useState('menu');
 
-	const [score, setScore] = useState(0);
+  const [score, setScore] = useState(0);
 
-	return (
-		<div className="App">
-			<h1 className="text-2xl py-8">Quiz App</h1>
-
-			<QuizContext.Provider
-				value={{ gameState, setGameState, score, setScore }}
-			>
-				<Layout>
-					{gameState === "menu" && <MainMenu />}
-					{gameState === "quiz" && <Quiz />}
-					{gameState === "endScreen" && <EndScreen />}
-				</Layout>
-			</QuizContext.Provider>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Navbar />
+      <h1 className="text-2xl py-8">You Don't Know Me - Mohamed Salah</h1>
+      <span>$ echo "I will Linux you !" </span>
+      <QuizContext.Provider
+        value={{ gameState, setGameState, score, setScore }}
+      >
+        <Layout>
+          {gameState === 'menu' && <MainMenu />}
+          {gameState === 'quiz' && <Quiz />}
+          {gameState === 'endScreen' && <EndScreen />}
+        </Layout>
+      </QuizContext.Provider>
+    </div>
+  );
 }
 
 export default App;
